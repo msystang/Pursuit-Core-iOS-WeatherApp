071208258c9d36387d3e31a65545450af4d70287
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-11 17:24:36 -0400

    adds empty Weather model file

---
+++ WeatherApp/WeatherApp/Models/Weather.swift
@@ -0,0 +1,13 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Sunni Tang on 10/11/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    
+}

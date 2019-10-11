f75eba06b823753d153ab4a760b7c0693ddee1b5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-11 17:15:57 -0400

    adds empty view controller and cell files

---
+++ WeatherApp/WeatherApp/Views/FavoriteImagesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteImagesTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Sunni Tang on 10/11/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteImagesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherApp/WeatherApp/Views/WeatherCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Sunni Tang on 10/11/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    
+}

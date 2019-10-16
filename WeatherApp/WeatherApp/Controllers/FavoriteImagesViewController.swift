a62e75f333e85d1b611c73cc53fb95d95fb17650
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-10-16 16:02:55 -0400

    adds loadFavorites func in favoriteImagesVC

--- WeatherApp/WeatherApp/Controllers/FavoriteImagesViewController.swift
+++ WeatherApp/WeatherApp/Controllers/FavoriteImagesViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 
 class FavoriteImagesViewController: UIViewController {
     
+    var favorites = [Image]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        loadFavorites()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadFavorites() {
+        do {
+            favorites = try ImagePersistenceHelper.manager.get()
+        } catch {
+            print(error)
+        }
+        
     }
-    */
-
+    
 }
